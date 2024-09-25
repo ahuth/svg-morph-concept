2a695865439cfc336aa04736b1d77054a0953988
Andrew Huth <git@huth.me>
2024-09-25 16:15:48 -0400

    Create an infinite number of handholds

--- src/components/Handholds.tsx
+++ src/components/Handholds.tsx
@@ -1,5 +1,7 @@
 import {animated, useSpringValue, type SpringValue} from '@react-spring/web';
-import {useEffect, useRef} from 'react';
+import {useEffect, useRef, useState} from 'react';
+import range from 'lodash/range';
+import reverse from 'lodash/reverse';
 
 type Props = {
   /**
@@ -10,12 +12,14 @@ type Props = {
 };
 
 export default function Handholds({increment}: Props) {
+  const [count, setCount] = useState(0);
   const y = useSpringValue(40);
   const isMountedRef = useRef(false);
 
   useEffect(() => {
+    setCount((val) => val + 1);
     if (isMountedRef.current) {
-      y.start(y.get() + 60);
+      y.start(y.get() + 50);
     }
   }, [increment]);
 
@@ -28,19 +32,28 @@ export default function Handholds({increment}: Props) {
 
   return (
     <>
-      <HandholdPair y={y} />
+      {makeReversedRange(count).map((num) => {
+        return <HandholdPair key={num} num={num} y={y} />;
+      })}
     </>
   );
 }
 
-function HandholdPair({y}: {y: SpringValue<number>}) {
+function HandholdPair({num, y}: {num: number; y: SpringValue<number>}) {
+  // The y value is for the lowest handhold, so offset it back up by which handhold this is.
+  const y1 = y.to((val) => val - num * 100);
+
   return (
     <>
       {/* prettier-ignore */}
-      <animated.ellipse cx="130" cy={y} rx="15" ry="10" fill="orange" stroke="black" strokeWidth="2"/>
+      <animated.ellipse cx="130" cy={y1} rx="15" ry="10" fill="orange" stroke="black" strokeWidth="2"/>
       {/* prettier-ignore */}
-      <animated.ellipse cx="70" cy={y.to((val) => val + 50)} rx="15" ry="10" fill="orange" stroke="black" strokeWidth="2"
+      <animated.ellipse cx="70" cy={y1.to((val) => val + 50)} rx="15" ry="10" fill="orange" stroke="black" strokeWidth="2"
       />
     </>
   );
+}
+
+function makeReversedRange(length: number) {
+  return reverse(range(length));
 }
